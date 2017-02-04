b3aa4d11975b1220d27425d2a142de702ec92a10
Holly Oegema <heoegema@Hollys-MacBook-Air.local>
2017-02-04 01:47:21 -0500

    Added app and basic templates.

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from wtforms import Form, TextAreaField, validators
+from flask_bootstrap import Bootstrap
+import indicoio
+
+import pygal
+
+indicoio.config.api_key = 'c938b911dce99664a3af0f077ad2edc6'
+app = Flask(__name__)
+Bootstrap(app)
+
+class HelloForm(Form):
+	sayhello = TextAreaField('',[validators.DataRequired()])
+
+@app.route('/')
+def index():
+	form = HelloForm(request.form)
+	return render_template('index.html', form=form)
+
+@app.route('/hello', methods=['POST'])
+def hello():
+	form = HelloForm(request.form)
+	if request.method == 'POST' and form.validate():
+		name = request.form['sayhello']
+		chart = ehwakte
+		result = indicoio.sentiment(name)
+		return render_template('hello.html', name=result, bar=chart)
+	return render_template('index.html', form=form)
+
+if __name__ == '__main__':
+	app.run(debug=True)
